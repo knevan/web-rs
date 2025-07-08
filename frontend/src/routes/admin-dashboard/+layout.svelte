5eb37ddfdb399f0a14d74cec1c5f5d24d9cc9661
wahyu <wahyuby.nev@gmail.com>
2025-07-09 02:55:02 +0700

    add admin dashboard
    - Initial work for admin dashboard with dummy data

---
+++ frontend/src/routes/admin-dashboard/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    let {children} = $props();
+</script>
+
+<div class="flex flex-col min-h-screen bg-gray-50">
+
+    <main class="flex-grow container mx-auto px-4">
+        {@render children()}
+    </main>
+
+</div>
