88a58ba2e28187c102f94d9dddf654428c5ad8b6
wahyu <wahyuby.nev@gmail.com>
2025-07-11 02:40:57 +0700

    add eye icon on input password

--- frontend/src/lib/components/LoginForm.svelte
+++ frontend/src/lib/components/LoginForm.svelte
@@ -59,7 +59,8 @@
                         </a>
                     </div>
                     <div class="relative">
-                        <Input bind:value={password} id="password-{id}" type="password"
+                        <Input bind:value={password} id="password-{id}"
+                               type={showPassword ? 'text' : 'password'}
                                required/>
                         <!-- Toggle button for password visibility -->
                         <button
@@ -69,28 +70,6 @@
                                 aria-label="Toggle password visibility"
                         >
                             {#if showPassword}
-                                <!-- Eye Off Icon -->
-                                <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        stroke-width="2"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                >
-
-                                    <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/>
-                                    <path
-                                            d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"
-                                    />
-                                    <path
-                                            d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"
-                                    />
-                                    <line x1="2" x2="22" y1="2" y2="22"/>
-                                </svg>
-                            {:else}
                                 <!-- Eye Icon -->
                                 <svg
                                         xmlns="http://www.w3.org/2000/svg"
@@ -108,6 +87,23 @@
                                             cy="12"
                                             r="3"
                                     />
+                                </svg>
+                            {:else}
+                                <!-- Eye Off Icon -->
+                                <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        stroke-width="2"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                >
+                                    <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/>
+                                    <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"/>
+                                    <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"/>
+                                    <line x1="2" x2="22" y1="2" y2="22"/>
                                 </svg>
                             {/if}
                         </button>
