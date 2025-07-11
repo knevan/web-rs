13600711903447abd7a8ad5032e555ac424db436
wahyu <wahyuby.nev@gmail.com>
2025-07-12 03:07:34 +0700

    add dynamic manga read page

---
+++ frontend/src/routes/manga/[manga_name]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import {page} from "$app/state";
+    import MangaView from "$lib/components/MangaView.svelte";
+
+    const mangaId = page.params['manga_name'];
+</script>
+
+<MangaView {mangaId}/>
