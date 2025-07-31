e0f4ea176e14b1d696ab419a364f808802f18e97
wahyu <wahyuby.nev@gmail.com>
2025-08-01 00:52:15 +0700

    Remove direct use inline svg icon

--- frontend/src/lib/components/SeriesCarousel.svelte
+++ frontend/src/lib/components/SeriesCarousel.svelte
@@ -4,6 +4,7 @@
     import type {EmblaCarouselType, EmblaOptionsType} from "embla-carousel";
     import Autoplay from 'embla-carousel-autoplay';
     import image_test from '$lib/images/image_image.webp';
+    import {ChevronLeft, ChevronRight} from "@lucide/svelte";
 
     // Define the props for the component
     interface Props {
@@ -86,12 +87,9 @@
 </script>
 
 <div class="manga-list-container">
-    <!-- Left scroll button -->
-    <button class="scroll-button left" onclick={scrollPrev} aria-label="Scroll left">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path fill="none" d="M0 0h24v24H0z"/>
-            <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" fill="currentColor"/>
-        </svg>
+    <!-- Left click button -->
+    <button class="click-button left" onclick={scrollPrev} aria-label="Scroll left">
+        <ChevronLeft/>
     </button>
 
     <!-- Embla Carousel root element -->
@@ -111,12 +109,9 @@
         </div>
     </div>
 
-    <!-- Right scroll button -->
-    <button class="scroll-button right" onclick={scrollNext} aria-label="Scroll right">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-            <path fill="none" d="M0 0h24v24H0z"/>
-            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" fill="currentColor"/>
-        </svg>
+    <!-- Right click button -->
+    <button class="click-button right" onclick={scrollNext} aria-label="Scroll right">
+        <ChevronRight/>
     </button>
 </div>
 
@@ -169,7 +164,7 @@
     }
 
     /* Styling for navigation buttons */
-    .scroll-button {
+    .click-button {
         position: absolute;
         top: 50%;
         /* Adjust transform to align with the image, not the whole container */
@@ -188,15 +183,15 @@
         transition: background-color 0.2s;
     }
 
-    .scroll-button.left {
-        left: -20px;
+    .click-button.left {
+        left: -50px;
     }
 
-    .scroll-button.right {
-        right: -20px;
+    .click-button.right {
+        right: -50px;
     }
 
-    .scroll-button:hover {
+    .click-button:hover {
         background-color: #ffffff;
     }
 </style>
