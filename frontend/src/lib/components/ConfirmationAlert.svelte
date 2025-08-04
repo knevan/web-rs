917456929b848b00cb07d2b152771439e6c3c445
wahyu <wahyuby.nev@gmail.com>
2025-08-04 13:44:52 +0700

    add confirmation dialog component

---
+++ frontend/src/lib/components/ConfirmationAlert.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js"
+
+    type Props = {
+        open: boolean;
+        title: string;
+        message: string;
+        onConfirm: () => void;
+        onCancel: () => void;
+    };
+
+    let {open, title, message, onConfirm, onCancel}: Props = $props();
+
+    function handleOpenChange(isOpen: boolean) {
+        if (!isOpen) {
+            onCancel();
+        }
+    }
+</script>
+
+<AlertDialog.Root bind:open onOpenChange={handleOpenChange}>
+    <AlertDialog.Content>
+        <AlertDialog.Header>
+            <AlertDialog.Title>{title}</AlertDialog.Title>
+            <AlertDialog.Description>
+                {message}
+            </AlertDialog.Description>
+        </AlertDialog.Header>
+        <AlertDialog.Footer>
+            <AlertDialog.Cancel onclick={onCancel}>
+                Cancel
+            </AlertDialog.Cancel>
+            <AlertDialog.Action onclick={onConfirm}>
+                Confirm
+            </AlertDialog.Action>
+        </AlertDialog.Footer>
+    </AlertDialog.Content>
+</AlertDialog.Root>
