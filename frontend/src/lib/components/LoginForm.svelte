250c3286b5657822fa3aa6e5f593854785cc3c0a
wahyu <wahyuby.nev@gmail.com>
2025-09-01 23:04:39 +0700

    adjust mobile layout

--- frontend/src/lib/components/LoginForm.svelte
+++ frontend/src/lib/components/LoginForm.svelte
@@ -33,14 +33,14 @@
             auth.update((store) => ({...store, error: null}));
         }
     });
-    
+
     // const id = $props.id();
 </script>
 
 <Card.Root class="mx-auto w-full max-w-sm">
     <Card.Header class="text-center">
-        <Card.Title class="text-4xl">Login</Card.Title>
-        <Card.Description class="text-md">Enter your email or username below to login to your account
+        <Card.Title class="text-2xl md:text-4xl">Login</Card.Title>
+        <Card.Description class="text-xs md:text-base">Enter your email or username below to login to your account
         </Card.Description>
     </Card.Header>
     <Card.Content>
@@ -52,14 +52,15 @@
                 <div class="grid gap-2">
                     <Label for="email-{id}" class="text-md">Credential</Label>
                     <Input bind:value={email} id="email-{id}" type="text"
-                           placeholder="example@gmail.com or username"
+                           class="text-xs md:text-lg"
+                           placeholder="ex@gmail.com or username"
                            required/>
                 </div>
                 <div class="grid gap-2">
                     <div class="flex items-center">
-                        <Label for="password-{id}" class="text-md">Password</Label>
+                        <Label for="password-{id}" class="text-base md:text-lg">Password</Label>
                         <a href="/reset-password"
-                           class="ml-auto inline-block text-md underline">
+                           class="ml-auto inline-block text-xs md:text-base underline">
                             Forgot your password?
                         </a>
                     </div>
@@ -84,14 +85,14 @@
                         </button>
                     </div>
                 </div>
-                <Button type="submit" class="w-full text-md" disabled={isLoading}>
-                    {isLoading ? 'Logging in...' : 'login'}
+                <Button type="submit" class="w-full text-base md:text-lg" disabled={isLoading}>
+                    {isLoading ? 'Logging in...' : 'Login'}
                 </Button>
             </div>
         </form>
-        <div class="mt-4 text-center text-md">
+        <div class="mt-4 text-center text-base md:text-lg">
             Don't have an account?
-            <a href="/signup" class="underline"> Sign up </a>
+            <a href="/signup" class="underline text-base md:text-lg"> Sign up </a>
         </div>
     </Card.Content>
 </Card.Root>
