dd3e05cc19198241dcf0b34e809d496c1a196c8d
wahyu <wahyuby.nev@gmail.com>
2025-09-11 02:53:58 +0700

    fix spoiler bug, update comment layout

--- frontend/src/lib/components/comments/CommentView.svelte
+++ frontend/src/lib/components/comments/CommentView.svelte
@@ -2,20 +2,47 @@
     import CommentForm from './CommentForm.svelte';
     import CommentView from './CommentView.svelte';
     import {Button} from "$lib/components/ui/button";
-    import {mountSpoilers, parseAndSanitize} from "$lib/utils/markdown";
+    import {mountSpoilers} from "$lib/utils/markdown";
     import {tick} from "svelte";
+    import {apiFetch} from "$lib/store/auth";
+    import {UserRound} from "@lucide/svelte";
+    import {ChevronUp, ChevronDown} from "@lucide/svelte";
 
-    let {comment, addReply} = $props();
+    let {comment, addReply, currentUser} = $props();
     let showReplyForm = $state(false);
     let contentContainer = $state<HTMLElement | null>(null);
+    let isEditing = $state(false);
 
-    const sanitizedContent = $derived(
-        parseAndSanitize(comment.content),
-    )
+    const isOwnerComment = $derived(currentUser?.username === comment.user.username);
+
+    const sanitizedContent = $derived(comment.content_html);
 
     function handleReplySubmit(contentText: string) {
         addReply(comment.id, contentText);
         showReplyForm = false;
+    }
+
+    async function handleUpdateSubmit(newMarkdown: string) {
+        try {
+            const response = await apiFetch(`/api/comments/${comment.id}/edit`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({content_markdown: newMarkdown})
+            });
+            if (!response.ok) {
+                console.error(response);
+                return;
+            }
+            const updatedComment = await response.json();
+            comment.content_html = updatedComment.new_html_content;
+            comment.content_markdown = newMarkdown;
+
+            isEditing = false;
+        } catch (error) {
+            console.error('An error occurred while updating the comment:', error);
+        }
     }
 
     $effect(() => {
@@ -26,51 +53,102 @@
             });
         }
     });
+
+    function formatRelativeTime(dateString: string) {
+        if (!dateString) return '';
+
+        const date = new Date(dateString);
+        const now = new Date();// Calculate the difference in seconds between now and the provided date
+        let seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+        const minutes = Math.floor(seconds / 60);
+        if (minutes < 1) return "Just now";
+
+        const hours = Math.floor(minutes / 60);
+        if (hours < 1) return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+
+        const days = Math.floor(hours / 24);
+        if (days < 1) return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+
+        return `${days} day${days > 1 ? 's' : ''} ago`;
+    }
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col gap-2">
     <div class="flex gap-3">
-        <img src={comment.user.avatar} alt="User Avatar" class="mt-1 h-9 w-9 rounded-full"/>
-        <div class="flex-1">
-            <div class="flex items-center gap-2">
-                <span class="font-bold text-zinc-800 dark:text-zinc-100">{comment.user.name}</span>
-                <span class="text-sm text-zinc-500 dark:text-zinc-400">{comment.timestamp}</span>
+        {#if comment.user.avatar_url}
+            <img src={comment.user.avatar_url} alt="User Avatar" class="mt-1 h-9 w-9 rounded-full"/>
+        {:else}
+            <div class="mt-1 flex h-9 w-9 rounded-full items-center justify-center bg-gray-200 dark:bg-gray-700">
+                <UserRound class="h-5 w-5 text-gray-500 dark:text-gray-400"/>
             </div>
+        {/if}
 
+        <div class="flex items-center gap-2">
+            <span class="font-bold text-zinc-800 dark:text-zinc-100">{comment.user.username}</span>
+            <span class="text-sm text-zinc-500 dark:text-zinc-400">{formatRelativeTime(comment.created_at)}</span>
+        </div>
+    </div>
+    <div class="flex-1">
+        {#if isEditing}
+            <div class="mt-2">
+                <CommentForm submitText={handleUpdateSubmit}
+                             initialContent={comment.content_markdown}
+                             submitLabel="Save Changes"
+                />
+                <Button variant="ghost" size="sm" onclick={() => (isEditing = false)} class="mt-1 text-xs">
+                    Cancel
+                </Button>
+            </div>
+        {:else}
             <div bind:this={contentContainer} class="prose prose-zinc mt-1 max-w-none dark:prose-invert">
                 {@html sanitizedContent}
             </div>
 
-            {#if comment.imageUrl}
+            <!-- {#if comment.imageUrl}
                 <img src={comment.imageUrl} alt="User content" class="mt-2 max-h-[250px] max-w-full rounded-lg"/>
-            {/if}
+            {/if} -->
 
-            <div class="comment-actions">
-                <Button class="text-sm bg-transparent text-gray-700 dark:text-gray-200">
-                    👍 {comment.upvotes}</Button>
-                <Button class="text-sm bg-transparent text-gray-700 dark:text-gray-200">👎</Button>
-                <Button class="text-sm font-medium bg-transparent text-gray-700 dark:text-gray-200"
-                        onclick={() => (showReplyForm = !showReplyForm)}>
-                    Reply to user
+            <div class="comment-actions mt-1 flex items-center gap-1">
+                <Button class="text-sm bg-transparent text-gray-700 dark:text-gray-200 hover:bg-transparent dark:hover:bg-transparent"
+                        size="sm">
+                    {comment.upvotes}
+                    <ChevronUp/>
                 </Button>
+                <Button class="text-sm bg-transparent text-gray-700 dark:text-gray-200 hover:bg-transparent dark:hover:bg-transparent"
+                        size="sm">
+                    <ChevronDown/>
+                </Button>
+                <Button class="text-sm font-medium bg-transparent text-gray-700 dark:text-gray-200 hover:bg-transparent dark:hover:bg-transparent"
+                        size="sm"
+                        onclick={() => (showReplyForm = !showReplyForm)}>
+                    Reply
+                </Button>
+                {#if isOwnerComment}
+                    <Button class="text-sm font-medium bg-transparent text-gray-700 dark:text-gray-200"
+                            onclick={() => (isEditing = true)}
+                    >
+                        Edit
+                    </Button>
+                {/if}
             </div>
-        </div>
+        {/if}
     </div>
 
-    {#if showReplyForm}
+    {#if showReplyForm && !isEditing}
         <div class="mt-3 pl-12">
             <CommentForm
                     submitText={handleReplySubmit}
-                    placeholder={`Reply to ${comment.user.name}`}
-                    submitLabel="Reply Send"
+                    placeholder={`Reply to ${comment.user.username}`}
+                    submitLabel="Reply"
             />
         </div>
     {/if}
 
     {#if comment.replies?.length > 0}
-        <div class="mt-4 border-l-2 border-zinc-200 pl-6 ml-[18px]">
+        <div class="mt-1 border-l-1 border-zinc-200 pl-2 ml-2">
             {#each comment.replies as reply (reply.id)}
-                <CommentView comment={reply} {addReply}/>
+                <CommentView {currentUser} comment={reply} {addReply}/>
             {/each}
         </div>
     {/if}
