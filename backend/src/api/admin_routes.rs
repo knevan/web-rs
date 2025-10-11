d5e4c344a8286529fa35f4e1339136ab6ee00d2c
wahyu <wahyuby.nev@gmail.com>
2025-10-11 20:56:31 +0700

    improve pagination search admin panel

--- backend/src/api/admin_routes.rs
+++ backend/src/api/admin_routes.rs
@@ -1,24 +1,31 @@
-use axum::Router;
 use axum::routing::{delete, get, patch, post};
+use axum::Router;
 
 use crate::api::admin_handlers::{
     create_category_tag_handler, create_new_series_handler,
-    delete_category_tag_handler, delete_series_handler, get_all_series_handler,
-    get_all_users_handler, get_list_category_tags_handler,
-    get_series_category_tags_handler, repair_chapter_handler,
-    update_existing_series_handler, upload_series_cover_image_handler,
+    delete_category_tag_handler, delete_series_handler,
+    get_all_paginated_series_handler, get_all_paginated_users_handler,
+    get_list_category_tags_handler, get_series_category_tags_handler,
+    repair_chapter_handler, update_existing_series_handler,
+    upload_series_cover_image_handler,
 };
 use crate::builder::startup::AppState;
 
 pub fn admin_routes() -> Router<AppState> {
     Router::new()
         // User management routes
-        .route("/users/list", get(get_all_users_handler))
+        .route(
+            "/users/paginated/list-search",
+            get(get_all_paginated_users_handler),
+        )
         // Series management routes
         .route("/series/add", post(create_new_series_handler))
         .route("/series/delete/{id}", delete(delete_series_handler))
         .route("/series/repair/chapter/{id}", post(repair_chapter_handler))
-        .route("/series/list", get(get_all_series_handler))
+        .route(
+            "/series/paginated/list-search",
+            get(get_all_paginated_series_handler),
+        )
         .route("/series/update/{id}", patch(update_existing_series_handler))
         .route("/series/tags/{id}", get(get_series_category_tags_handler))
         // Image upload routes
