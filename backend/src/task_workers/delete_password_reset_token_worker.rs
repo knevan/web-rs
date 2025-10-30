212cba2ee98586aadd6e0ef8d1a68df33a2a1d8c
wahyu <wahyuby.nev@gmail.com>
2025-10-31 02:28:29 +0700

    add delete reset password token background worker

--- backend/src/task_workers/delete_password_reset_token_worker.rs
+++ backend/src/task_workers/delete_password_reset_token_worker.rs
@@ -1,16 +1,15 @@
 use std::time::Duration;
+
 use crate::database::DatabaseService;
 
-pub async fn run_cleanup_password_reset_token_worker(
-    db_service: DatabaseService
-) {
+pub async fn run_cleanup_password_reset_token_worker(db_service: DatabaseService) {
     let mut interval = tokio::time::interval(Duration::from_mins(10));
-    
+
     interval.tick().await;
-    
-    loop  {
+
+    loop {
         interval.tick().await;
-        
+
         match db_service.cleanup_password_reset_token().await {
             Ok(delete_token) => {
                 if delete_token > 0 {
