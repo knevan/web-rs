b3abc64fc5038b6929b8f82239c26e15eda9fcb0
wahyu <wahyuby.nev@gmail.com>
2025-11-09 02:24:38 +0700

    fix reduntant semantic html

--- frontend/src/routes/user/+layout.svelte
+++ frontend/src/routes/user/+layout.svelte
@@ -2,9 +2,8 @@
     let {children} = $props();
 </script>
 
-<div class="flex min-h-screen flex-col">
-    <main class="max-w-7xl p-4">
+<div class="flex min-h-screen">
+    <main class="max-w-7xl p-4 flex-1 mx-auto">
         {@render children()}
     </main>
-
 </div>
